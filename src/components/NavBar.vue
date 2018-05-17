51fec5068d87fb55e0daec56b3f6a92596268a78
brankaDakic10 <branka.dakic@yahoo.com>
2018-05-17 09:49:21 +0200

    Added 1.Task

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+   <div>
+  <b-navbar type="dark" variant="success" toggleable>
+    <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
+    <b-collapse is-nav id="nav_dropdown_collapse">
+      <b-navbar-nav>
+        <router-link class="navbar-brand" :to="{name: 'home'}">MoviesApp</router-link>
+        <router-link class="nav-item nav-link" :to="{name: 'movies'}">Movies</router-link>
+       
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
